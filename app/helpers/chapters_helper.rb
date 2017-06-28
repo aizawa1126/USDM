6a6b919bd21033ac6644f4b9abd0868ffdda3832
四十澤達也 <aizawa_tatsuya@yahoo.co.jp>
2017-06-29 00:10:03 +0900

    章内の表示をプレーンテキストからテーブルに置き換え

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,2 +1,16 @@
 module ChaptersHelper
+  def rowspan section_id
+    row_count = 0
+    section = @chapter.sections.find(section_id)
+    section.requirements.order("number").each do |requirement|
+      row_count += 3
+      requirement.functions.each do |function|
+        row_count += 1
+        function.specs.each do |spec|
+          row_count += 1
+        end
+      end
+    end
+    row_count
+  end
 end
