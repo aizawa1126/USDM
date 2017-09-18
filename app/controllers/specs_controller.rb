fddc6d43952b5c465d742e53d6cefcc04ccb2b5c
四十澤達也 <aizawa_tatsuya@yahoo.co.jp>
2017-09-18 19:55:57 +0900

    仕様番号を変更可能に修正

--- app/controllers/specs_controller.rb
+++ app/controllers/specs_controller.rb
@@ -78,6 +78,6 @@ class SpecsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def spec_params
-      params.require(:spec).permit(:content, :function_id, :customer_review, :design_review, :test_review)
+      params.require(:spec).permit(:number, :content, :function_id, :customer_review, :design_review, :test_review)
     end
 end
