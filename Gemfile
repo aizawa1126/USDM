a8d9dfe6d47d66afcdf5c7691d62dc8d429f49f3
四十澤達也 <aizawa_tatsuya@yahoo.co.jp>
2017-10-21 02:04:24 +0900

    マークダウン対応

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'wkhtmltopdf-binary'
 gem 'wicked_pdf'
 gem 'bcrypt'
+gem 'redcarpet', '~> 2.3.0'
+gem 'coderay'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  require "redcarpet"
+  require "coderay"
+
   def has_operation? operation, specification
     return false if current_user.nil?
     member = find_member(specification)
@@ -16,6 +19,21 @@ module ApplicationHelper
 
   def admin?
     current_user && current_user.admin
+  end
+
+  @@markdown = Redcarpet::Markdown.new Redcarpet::Render::HTML,
+    autolink: true,
+    space_after_headers: true,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    tables: true,
+    hard_wrap: true,
+    xhtml: true,
+    lax_html_blocks: true,
+    strikethrough: true
+
+  def markdown(content)
+    @@markdown.render(content).html_safe
   end
 
   private
