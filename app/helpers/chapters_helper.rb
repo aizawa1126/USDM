d9e699c8819a46b524b1b976bc42007bea3838d1
四十澤達也 <aizawa_tatsuya@yahoo.co.jp>
2018-01-21 12:37:04 +0900

    仕様書全体のPDF出力に対応

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -25,9 +25,8 @@ module ChaptersHelper
     row_count
   end
 
-    def rowspan_pdf section_id
+  def rowspan_pdf section
     row_count = 0
-    section = @chapter.sections.find(section_id)
     section.requirements.order("number").each do |requirement|
       row_count += 3
       requirement.functions.each do |function|
